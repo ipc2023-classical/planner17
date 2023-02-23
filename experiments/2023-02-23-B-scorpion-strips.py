b5d9d79be70b4480fe7ce6e147f090923e9b17db
Jendrik Seipp <jendrik.seipp@liu.se>
2023-02-23 22:13:43 +0100

    Adapt Scorpion experiment to nested-repos setting.

--- experiments/2023-02-23-B-scorpion-strips.py
+++ experiments/2023-02-23-B-scorpion-strips.py
@@ -1,37 +1,40 @@
 #! /usr/bin/env python
 
+from dataclasses import dataclass
 from pathlib import Path
 import shutil
+import subprocess
+import sys
 
 from downward import suites
-from downward.cached_revision import CachedFastDownwardRevision
 from downward.experiment import (
     _DownwardAlgorithm,
     _get_solver_resource_name,
     FastDownwardRun,
 )
-from lab.experiment import Experiment, get_default_data_dir
+from lab.cached_revision import get_global_rev
+from lab.experiment import Experiment
 
 import project
 
 
 REPO = project.get_repo_base()
 PLANNER = REPO / "planners" / "scorpion"
-BENCHMARKS_DIR = REPO / "benchmarks"
+BENCHMARKS_DIR = REPO / "benchmarks" / "pddl"  # TODO: use SAS+ versions directly
 SCP_LOGIN = "nsc"
 REMOTE_REPOS_DIR = "/proj/dfsplan/users/x_jense/"
 #SUITE = ["depot:p01.pddl", "grid:prob01.pddl", "gripper:prob01.pddl"]
 SUITE = ["miconic-strips:0-p01.pddl"]
-REVISION_CACHE = Path(get_default_data_dir()) / "revision-cache"
 if project.REMOTE:
     ENV = project.BaselSlurmEnvironment(email="jendrik.seipp@liu.se", memory_per_cpu="9G", partition="infai_2")
     SUITE = project.SUITE_STRIPS
 else:
     ENV = project.LocalEnvironment(processes=2)
 
+SYS_SCP_MAX_TIME = 60 if project.REMOTE else 1
 CONFIGS = [
     ("scorpion", ["--search", "astar(scp_online(["
-        "projections(sys_scp(max_time=60, max_time_per_restart=6)), "
+        f"projections(sys_scp(max_time={SYS_SCP_MAX_TIME}, max_time_per_restart=6)), "
         "cartesian()], "
         "saturator=perimstar, max_time=60, interval=10K, orders=greedy_orders()), "
         "pruning=limited_pruning(pruning=atom_centric_stubborn_sets(), min_required_pruning_ratio=0.2))"]),
@@ -45,10 +48,6 @@ DRIVER_OPTIONS = [
     # Same memory limit as in competition.
     "--overall-memory-limit", "8G",
 ]
-# Pairs of revision identifier and revision nick.
-REVS = [
-    ("TODO", ""),
-]
 ATTRIBUTES = [
     "error",
     "run_dir",
@@ -61,31 +60,37 @@ ATTRIBUTES = [
     #"memory",
 ]
 
-exp = Experiment(environment=ENV)
-for rev, rev_nick in REVS:
-    cached_rev = CachedFastDownwardRevision(PLANNER, rev, BUILD_OPTIONS)
-    cached_rev.cache(REVISION_CACHE)
-    cache_path = REVISION_CACHE / cached_rev.name
-    dest_path = Path(f"code-{cached_rev.name}")
-    exp.add_resource("", cache_path, dest_path)
-    # Overwrite the script to set an environment variable.
-    exp.add_resource(
-        _get_solver_resource_name(cached_rev),
-        cache_path / "fast-downward.py",
-        dest_path / "fast-downward.py",
-    )
-    for config_nick, config in CONFIGS:
-        algo_name = f"{rev_nick}:{config_nick}" if rev_nick else config_nick
+subprocess.run([sys.executable, "build.py"] + BUILD_OPTIONS, cwd=PLANNER)
 
-        for task in suites.build_suite(BENCHMARKS_DIR, SUITE):
-            algo = _DownwardAlgorithm(
-                algo_name,
-                cached_rev,
-                DRIVER_OPTIONS,
-                config,
-            )
-            run = FastDownwardRun(exp, algo, task)
-            exp.add_run(run)
+exp = Experiment(environment=ENV)
+
+@dataclass
+class MockCachedRevision:
+    name: str
+    repo: str
+    local_rev: str
+    global_rev: str
+    build_options: list[str]
+
+rev = "ipc2023-classical"
+cached_rev = MockCachedRevision(
+    name="scorpion", repo=str(PLANNER), local_rev=rev, global_rev=get_global_rev(REPO, rev=rev), build_options=BUILD_OPTIONS)
+
+exp.add_resource("", PLANNER / "driver", "code/driver")
+exp.add_resource(_get_solver_resource_name(cached_rev), PLANNER / "fast-downward.py", "code/fast-downward.py")
+exp.add_resource("", PLANNER / "builds" / "release" / "bin", "code/builds/release/bin")
+
+for config_nick, config in CONFIGS:
+
+    for task in suites.build_suite(BENCHMARKS_DIR, SUITE):
+        algo = _DownwardAlgorithm(
+            f"{cached_rev.name}:{config_nick}",
+            cached_rev,
+            DRIVER_OPTIONS,
+            config,
+        )
+        run = FastDownwardRun(exp, algo, task)
+        exp.add_run(run)
 
 exp.add_parser(project.FastDownwardExperiment.EXITCODE_PARSER)
 #exp.add_parser(project.FastDownwardExperiment.TRANSLATOR_PARSER)
