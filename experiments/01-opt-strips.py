eb37de26525b3075e36e4ca2aa8894e94d4d06e0
Jendrik Seipp <jendrik.seipp@liu.se>
2023-03-16 22:01:45 +0100

    Prepare Scorpion experiment.

--- experiments/01-opt-strips.py
+++ experiments/01-opt-strips.py
@@ -13,7 +13,6 @@ BENCHMARKS_DIR = REPO / "benchmarks"
 assert BENCHMARKS_DIR.exists(), BENCHMARKS_DIR
 ATTRIBUTES = [
     "error",
-    "run_dir",
     "coverage",
     "cost",
     "memory",
@@ -23,21 +22,21 @@ ATTRIBUTES = [
 exp = Experiment()
 exp.add_step("remove-combined-properties", project.remove_properties, Path(exp.eval_dir))
 
-project.fetch_algorithms(exp, "2023-03-04-B-odin-strips-30m", filters=[project.strip_properties])
-project.fetch_algorithms(exp, "2023-03-05-B-decstar-strips", filters=[project.strip_properties])
-project.fetch_algorithms(exp, "2023-03-06-A-blind-hmax-strips-30m", filters=[project.strip_properties])
-project.fetch_algorithms(exp, "2023-03-06-C-dofri-strips", filters=[project.strip_properties])
-project.fetch_algorithms(exp, "2023-03-07-A-symk-strips-30m", filters=[project.strip_properties])
+for expname in [
+    "2023-03-04-B-odin-strips-30m",
+    "2023-03-05-B-decstar-strips",
+    "2023-03-06-A-blind-hmax-strips-30m",
+    "2023-03-06-C-dofri-strips",
+    "2023-03-07-A-symk-strips-30m",
+]:
+    project.fetch_algorithms(exp, expname, filters=[project.strip_properties])
 
 project.add_absolute_report(
     exp, attributes=ATTRIBUTES, name=f"{exp.name}-full"
 )
 
-exp.add_report(project.Hardest30Report(), outfile="properties", name="keep-only-hardest-tasks-in-properties")
-exp.add_step("compress-properties", project.compress_properties, Path(exp.eval_dir))
-
-project.add_absolute_report(
-    exp, attributes=ATTRIBUTES, name=f"{exp.name}-hardest"
-)
+exp.add_report(project.Hardest30Report(), outfile="properties-hardest.json", name="keep-only-30-hardest-tasks")
+#exp.add_step("compress-properties", project.compress_properties, Path(exp.eval_dir))
+#project.add_absolute_report(exp, attributes=ATTRIBUTES, name=f"{exp.name}-hardest")
 
 exp.run_steps()
