0d2d1e78404f221901f274df300e5f9573c3f05f
Jendrik Seipp <jendrik.seipp@liu.se>
2023-03-17 08:35:56 +0100

    Add initial training results.

--- learners/learn-portfolios.sh
+++ learners/learn-portfolios.sh
@@ -4,8 +4,9 @@ set -euo pipefail
 
 STRIPS_DATA=../experiments/data/01-opt-strips-eval/properties-hardest.json.xz
 
-./stonesoup.py --track opt ${STRIPS_DATA} 110 | tee stonesoup-opt-strips.txt
-#./stonesoup.py --track opt ../experiments/data/02-opt-configs-cond-effs-eval/properties.xz 120 | tee stonesoup-opt-cond-effs.txt
+#./batch-stonesoup.sh ${STRIPS_DATA} opt 1800 | tee batch-stonesoup-opt-strips.txt
+./stonesoup.py --track opt ${STRIPS_DATA} 90 | tee stonesoup-opt-strips.txt
+./greedy.py ${STRIPS_DATA} --track opt | tee greedy-opt-strips.txt
 
-#./greedy.py ../experiments//data/02-opt-configs-cond-effs-eval/properties.xz --track opt | tee greedy-opt-strips.txt
+#./stonesoup.py --track opt ../experiments/data/02-opt-configs-cond-effs-eval/properties.xz 120 | tee stonesoup-opt-cond-effs.txt
 #./greedy.py ../experiments/ipc2023/data/02-opt-configs-cond-effs-eval/properties.xz --track opt | tee greedy-opt-cond-effs.txt
