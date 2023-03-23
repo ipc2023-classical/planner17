9478c1c1aa757f5f3e5f5f7b1e9c615fca9dd72a
Jendrik Seipp <jendrik.seipp@liu.se>
2023-03-23 15:21:44 +0100

    Update portfolio.

--- driver/portfolios/seq_opt_ragnarok.py
+++ driver/portfolios/seq_opt_ragnarok.py
@@ -1,11 +1,13 @@
 SAS_FILE = "output.sas"
 OPTIMAL = True
 
-CONFIGS_STRIPS = [
+CONFIGS_STRIPS =  [
     # scorpion:sys-scp-interesting_non_negative-60s-cartesian-single
-    (987, ['scorpion', '--search', 'astar(scp_online([projections(sys_scp(max_time=60, max_time_per_restart=6, max_pdb_size=2M, max_collection_size=20M, pattern_type=interesting_non_negative)), cartesian(subtasks=[landmarks(order=random), goals(order=random)], max_states=infinity, max_transitions=1M, max_time=60, pick_flawed_abstract_state=first_on_shortest_path, pick_split=max_refined, tiebreak_split=min_cg, search_strategy=incremental)], saturator=perimstar, max_time=60, max_size=1M, interval=10K, orders=greedy_orders()), pruning=limited_pruning(pruning=atom_centric_stubborn_sets(), min_required_pruning_ratio=0.2))']),
+    (898, ['scorpion', '--search', 'astar(scp_online([projections(sys_scp(max_time=60, max_time_per_restart=6, max_pdb_size=2M, max_collection_size=20M, pattern_type=interesting_non_negative)), cartesian(subtasks=[landmarks(order=random), goals(order=random)], max_states=infinity, max_transitions=1M, max_time=60, pick_flawed_abstract_state=first_on_shortest_path, pick_split=max_refined, tiebreak_split=min_cg, search_strategy=incremental)], saturator=perimstar, max_time=60, max_size=1M, interval=10K, orders=greedy_orders()), pruning=limited_pruning(pruning=atom_centric_stubborn_sets(), min_required_pruning_ratio=0.2))']),
     # symk:sym-bd
     (716, ['symk', '--search', 'sym-bd(silent=true)']),
+    # decstar:A*-oss-sss-LMc
+    (15, ['decstar', '--search', 'astar(lmcut, pruning=stubborn_sets_ec(min_pruning_ratio=0.25), symmetry=symmetry_state_pruning)']),
     # decstar:ds-sss-oss
     (79, ['decstar', '--decoupling', 'lp_general(factoring_time_limit=30, memory_limit=7000, add_cg_sccs=true, strategy=mm_approx, min_flexibility=0.8)', '--search', 'astar(lmcut, pruning=stubborn_sets_decoupled(min_pruning_ratio=0.25, special_case_optimizations=true, use_single_var_ifork_optimization=true), symmetry=symmetry_state_pruning(lex_prices=true, lex_num_states=false, lex_goal_cost=false))']),
 ]
